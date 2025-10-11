7c36ed70948835259344f2635b34c991cc3c15d3
Davi Gabriel <davig1314@gmail.com>
2025-10-11 12:46:38 -0300

    Remove arquivo .env.example e ajusta configuração SSL para permitir conexões não autorizadas

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const db = mysql.createConnection({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT || 3306,
-  ssl: { rejectUnauthorized: true }
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect((err) => {
